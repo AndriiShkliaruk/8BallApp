ec744a9c89a880eca080f09f93b8a8a1e0b5c907
Andrii Shkliaruk <devshkliaruk@gmail.com>
2022-01-15 23:26:14 +0200

    Call to API and response representation added

--- 8BallApp/Controllers/BallViewController.swift
+++ 8BallApp/Controllers/BallViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class BallViewController: UIViewController {
-    let ballView = BallView()
+    
+    private let ballView = BallView()
+    private let callToShakeText = "Shake to see the answer"
+    private var answer = ""
+    let tempMessage = "No connection"
     
     override func loadView() {
         view = ballView
@@ -18,9 +23,45 @@ class BallViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        ballView.messageLabel.text = "Shake to see the answer"
+        ballView.messageLabel.text = callToShakeText
+        
+        
     }
     
+    private func updateMessageLabel(message: String) {
+        DispatchQueue.main.async {
+            self.ballView.messageLabel.text = message
+        }
+    }
     
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateMessageLabel(message: "")
+        
+        let url = URL(string: ballApiURL)
+        DataFetcher.get(from: url!) { (result: Result<BallResponse, DataError>) in
+            switch result {
+            case .failure:
+                self.answer = self.tempMessage
+            case .success(let results):
+                self.answer = results.magic.answer
+            }
+        }
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        UIDevice.vibrate()
+        updateMessageLabel(message: answer)
+    }
+
+    override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateMessageLabel(message: callToShakeText)
+    }
+    
+}
+
+extension UIDevice {
+    static func vibrate() {
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+    }
 }
 
