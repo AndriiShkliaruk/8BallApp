98f8ba949c7d775aa4968efe48368d091218f6c3
Andrii Shkliaruk <devshkliaruk@gmail.com>
2022-01-17 18:26:07 +0200

    BallViewController updated

--- 8BallApp/Controllers/BallViewController.swift
+++ 8BallApp/Controllers/BallViewController.swift
@@ -12,9 +12,11 @@ class BallViewController: UIViewController {
     
     private let ballView = BallView()
     private let callToShakeText = "Shake to see the answer"
-    private var answer = ""
-    let tempMessage = "No connection"
     
+    private lazy var settingsBarButtonItem: UIBarButtonItem = {
+        let gearIcon = UIImage(systemName: "gearshape.fill")
+        return UIBarButtonItem(image: gearIcon, style: .plain, target: self, action: #selector(settingsBarButtonTapped))
+    }()
     
     override func loadView() {
         view = ballView
@@ -24,6 +26,12 @@ class BallViewController: UIViewController {
         super.viewDidLoad()
         
         ballView.messageLabel.text = callToShakeText
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.tintColor = .gray
+        navigationItem.rightBarButtonItem = settingsBarButtonItem
     }
     
     private func updateMessageLabel(message: String) {
@@ -32,30 +40,46 @@ class BallViewController: UIViewController {
         }
     }
     
+    private func getRandomAnswer() -> String {
+        if let storedAnswer = StoredAnswers.load().randomElement() {
+            return storedAnswer
+        } else {
+            return defaultAnswers.randomElement()!
+        }
+    }
     
-    // MARK: - User interaction events
+    
+    // MARK: - User interaction methods
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        updateMessageLabel(message: "")
-        
-        let url = URL(string: ballApiURL)
-        DataFetcher.get(from: url!) { (result: Result<BallResponse, DataError>) in
-            switch result {
-            case .failure:
-                self.answer = self.tempMessage
-            case .success(let results):
-                self.answer = results.magic.answer
-            }
+        if motion == .motionShake {
+            updateMessageLabel(message: "")
         }
     }
 
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        UIDevice.vibrate()
-        updateMessageLabel(message: answer)
+        if motion == .motionShake {
+            let url = URL(string: ballApiURL)
+            DataFetcher.get(from: url!) { (result: Result<BallResponse, DataError>) in
+                switch result {
+                case .failure:
+                    
+                    self.updateMessageLabel(message: self.getRandomAnswer())
+                case .success(let results):
+                    self.updateMessageLabel(message: results.magic.answer)
+                }
+            }
+            UIDevice.vibrate()
+        }
     }
 
     override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         updateMessageLabel(message: callToShakeText)
+    }
+    
+    @objc func settingsBarButtonTapped(_ sender:UIButton!) {
+        let settingsViewController = SettingsViewController()
+        navigationController?.pushViewController(settingsViewController, animated: true)
     }
     
 }
